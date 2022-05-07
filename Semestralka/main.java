50956590d13d8f4a0953f4211fe75cf5234c3369
TobMit <80719513+TobMit@users.noreply.github.com>
2022-05-07 16:47:28 +0200

    Dokončenie semestralky.

--- Semestralka/main.java
+++ Semestralka/main.java
@@ -1,38 +1,133 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FilterInputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.Stack;
 
 public class main {
     public static int n = 500;
     public static int r = 350;
     public static int K =10500;
-    private static  int b_A_C[][] = new int[n][3];
+    private static  int b_C_A[][] = new int[n][3];
+    private static Stack<Integer> indexSameCValue = new Stack<>();
 
+
+    // Primárna heuristika -> začína prípustným k lepšiemu prípustnému
+    // Môj prípad -> plný batoh (počet prvkov je väčší aj kapacita je väčšia ako r a K) -> zmenšovanie počtu prvkov a hmotnosť
+    // Nespracované dáta sú všetky v batohu -> lokálne krytérium: Vyber z batohu najväčší C
 
     public static void main(String[] args) throws FileNotFoundException {
         nacitajData();
+        int plnyBatohHmotnost = spocitajHmotnostBatohu();
+        int plnyBatohCena = spocitajFx();
 
-        for (int[] ints : b_A_C) {
-            System.out.printf("%d %d %d\n", ints[0], ints[1], ints[2]);
+        //súčasne riešenie
+        int hodnotaFx = spocitajFx();
+        // súčasná kapacita
+        int az = spocitajHmotnostBatohu(); // kapacita batohu
+        // počet prvkov v batohu
+        int z = n;
+
+        System.out.printf("PocetPoloziek v batohu: %d\n", pocetPoloziekVBatohu());
+
+        // prípadne r + 1 keďže by sa mohlo stať že program skončí s 349 prvkami
+        while (z >= r + 1) {
+            if (indexSameCValue.isEmpty()){
+                napltStack();
+            }
+            int index = indexSameCValue.pop();
+            int newAz = az - b_C_A[index][2];
+            if (newAz < K) {
+                break;
+            }
+            az = newAz;
+            b_C_A[index][0] = 0;
+            z--;
         }
+
+        ulozData();
+
+        hodnotaFx = spocitajFx();
+        System.out.printf("Plny batoh: Fx: %d\tHmotnost: %d\n", plnyBatohCena, plnyBatohHmotnost);
+        System.out.printf("Heurestika: Fx: %d\tHmotnost: %d\n", hodnotaFx, spocitajHmotnostBatohu());
+        System.out.printf("PocetPoloziek v batohu: %d", pocetPoloziekVBatohu());
 
     }
 
+    private static void ulozData() throws FileNotFoundException {
+        PrintWriter zapisovac = new PrintWriter(new File("vystup.csv"));
+        zapisovac.println("sep=,"); // aby excel správne rozližil udaje do stlpcov
+        zapisovac.println("Vektor,C,A");
+
+        for (int i = 0; i < n; i++) {
+            zapisovac.printf("%d,%d,%d\n",b_C_A[i][0],b_C_A[i][1],b_C_A[i][2]);
+        }
+        zapisovac.close();
+    }
+
+    private static void napltStack() {
+        int maxCvalue = najdiMaxC();
+        for (int i = 0; i < n ; i++) {
+            if (maxCvalue == b_C_A[i][1] ){
+                indexSameCValue.push(i);
+            }
+        }
+    }
+
+    private static int najdiMaxC() {
+        int returnValue = 0;
+        for (int i = 0; i < n ; i++) {
+            if (returnValue < b_C_A[i][1] && b_C_A[i][0] == 1){
+                returnValue = b_C_A[i][1];
+            }
+        }
+        return returnValue;
+    }
+
+
+    private static int pocetPoloziekVBatohu() {
+        int returnValue = 0;
+        for (int[] ints : b_C_A) {
+            if (ints[0] == 1){
+                returnValue++;
+            }
+        }
+        return  returnValue;
+    }
+
+
+    private static int spocitajHmotnostBatohu() {
+        int retValue = 0;
+        for (int[] ints : b_C_A) {
+            if (ints[0] == 1){
+                retValue += ints[2];
+            }
+        }
+        return retValue;
+    }
+
+    private static int spocitajFx() {
+        int retValue = 0;
+        for (int[] ints : b_C_A) {
+            if (ints[0] == 1){
+                retValue += ints[1];
+            }
+        }
+        return retValue;
+    }
+
     private static void nacitajData() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("Semestralka/H4_c.txt"));
-        Scanner scanner1 =  new Scanner(new File("Semestralka/H4_a.txt"));
+        Scanner scannerC = new Scanner(new File("Semestralka/H4_c.txt"));
+        Scanner scannerA =  new Scanner(new File("Semestralka/H4_a.txt"));
         int index  = 0;
-        while (scanner.hasNextInt() && scanner1.hasNext()) {
-            b_A_C[index][0] = 0;
-            b_A_C[index][1] = scanner.nextInt();
-            b_A_C[index][2] = scanner1.nextInt();
+        while (scannerC.hasNextInt() && scannerA.hasNext()) {
+            b_C_A[index][0] = 1;
+            b_C_A[index][1] = scannerC.nextInt();
+            b_C_A[index][2] = scannerA.nextInt();
             index++;
 
         }
-        scanner.close();
-        scanner1.close();
+        scannerC.close();
+        scannerA.close();
     }
 }
